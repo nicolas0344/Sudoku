d58c7a70a86a443144f878f164e8ce1e60353531
nicolas0344 <nicolas.pralon@etu.umontpellier.fr>
2022-02-22 11:14:43 +0100

    à finir

--- Résolution_Sudoku.R
+++ Résolution_Sudoku.R
@@ -1,5 +1,6 @@
 library(combinat)
 
+
 bloc_ij<-function(i,j){
   x<-c(0,0)
   for (k in seq(1,7,by=3)){
@@ -13,8 +14,11 @@ bloc_ij<-function(i,j){
   return(x)
 }
 
-
 solutions_case<-function(M,i,j){
+  if (!(is.na(M[i,j]))){
+    return(M[i,j])
+  }
+  
   x <- bloc_ij(i,j)
   a <- x[1]
   b <- x[2]
@@ -25,7 +29,20 @@ solutions_case<-function(M,i,j){
   return(9-nlevels(c))
 }
 
-M <- Grille_incomplete(Sudoku(),30)
-solutions_case(M,1,3)
 
+M <- Grille_incomplete(Sudoku(),30)
+solutions_case(M,8,9)
+
+
+index_Grille<-function(M){
+  for (i in 1:9){
+    for (j in 1:9){
+      M[i,j] <- solutions_case(M,i,j)
+    }
+  }
+  return(M)
+}
+
+solutions_case(M,2,9)
+index_Grille(M)
 
