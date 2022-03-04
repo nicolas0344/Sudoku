b4cfdf2a4d9f5ac6caf914ee957bed9c0d872a8b
nicolas0344 <nicolas.pralon@etu.umontpellier.fr>
2022-03-04 23:39:28 +0100

    blabla

--- Shiny_Sudoku/server.R
+++ Shiny_Sudoku/server.R
@@ -5,12 +5,10 @@ shinyServer(function(input, output) {
     x <- reactiveValues(M = matrix(0,9,9))
     
     observeEvent(input$setButton, {
-        if (is.na(x$M)){
-            return(x$M[as.integer(input$row),as.integer(input$col)] <- as.integer(input$value)
-                   )
-        }
+        x$M[as.integer(input$row),as.integer(input$col)] <- as.integer(input$value)
     })
-    observeEvent(input$Game, {x$M = New_Game()})
+    observeEvent(input$Game, {x$M = Difficulté(input$Difficulty)})
+
     
     
     New_Game <- eventReactive(
